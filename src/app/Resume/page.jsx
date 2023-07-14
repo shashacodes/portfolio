3ce43716aa8bf4094e9b84936cf5ba0e145edb94
Ibanga Sharon <ibsharon19@gmail.com>
2023-07-14 12:49:18 +0100

    feat: add new changes

--- src/app/Resume/page.jsx
+++ src/app/Resume/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import cv from "../Contact/Images/cv.jpg";
+import cv from "../Contact/Images/cv1.jpg";
 
 export default function page() {
   return (
